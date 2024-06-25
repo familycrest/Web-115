71821afa895283d2b89bedd914d8ee331e05eb83
Kenneth Stancil <githubbed@proton.me>
2024-06-24 21:26:13 -0400

    First commit

---
+++ test.js
@@ -0,0 +1,23 @@
+let out = document.getElementById("loop");
+let interv;
+let startButton = document.getElementById("start");
+let stopButton = document.getElementById("stop");
+
+startButton.addEventListener("click", start);
+stopButton.addEventListener("click", stop);
+
+function printLoop() {
+    loop.innerHTML += "<br>Hi there<br>"
+}
+
+function start() {
+    if (!interv)
+        interv = window.setInterval(printLoop, 1000);
+}
+
+function stop() {
+    if (interv) {
+        window.clearInterval(interv);
+        interv = undefined;
+    }
+}
